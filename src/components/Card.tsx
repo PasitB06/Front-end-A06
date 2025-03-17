33b36cecde89f170867daebf67a0eb52161842ea
your_userName <example@email.com>
2025-03-17 23:58:51 +0700

    ฟดหกฟหกฟห

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,30 +1,37 @@
-import Image from 'next/image'
-import InteractiveCard from './InteractiveCard';
-import { Rating } from '@mui/material';
+import Image from "next/image";
+import InteractiveCard from "./InteractiveCard";
+import Rating from "@mui/material/Rating";
 
-export default function Card ({ venueName, imgSrc, onCompare, onRating }:{ venueName:string, imgSrc:string, onCompare:Function, onRating:Function}) {
+
+export default function Card ({venueName,imgSrc,ratingFunc}:{venueName:string,imgSrc:string,ratingFunc:Function}){
+    function onCompare(venueName: string) {
+        throw new Error("Function not implemented.");
+    }
 
     return (
         <InteractiveCard contentName={venueName}>
-            <div className="w-full h-[70%] relative rounded-t-lg">
-                <Image src={imgSrc}
-                    alt='Product Picture'
-                    fill={true}
-                    className='object-cover rounded-t-lg'
-                    objectFit='cover' />
-            </div>
-            <div className="w-full h-[15%] p-[10px]">{venueName}</div>
-            <Rating 
-            data-testid={`${venueName} Rating`} 
-            id={`${venueName} Rating`} 
-            name={`${venueName} Rating`}
-            className='mx-2 px-1 py-1' 
-            defaultValue={0} 
-            precision={1} 
 
-            onChange={(e, newValue) => {onRating(venueName, newValue)}}
-            onClick={(e) => {e.stopPropagation(); onCompare(venueName)}}
-            />
+            <div className="w-full h-[70%] rounded-t-lg relative text-center " >
+                    <Image 
+                        src={imgSrc}
+                        alt={venueName}
+                        fill={true}
+                        objectFit="cover"
+                    />
+                </div>
+
+                <div className="w-full h-[15%] p-[10px] ">
+                    <h1>{venueName}</h1>
+                </div>
+
+                <Rating size="large" name={${venueName} Rating} id={${venueName} Rating} data-testid={${venueName} Rating} className="w-fit h-[15%]" 
+                    onChange={(event, value) => {ratingFunc(venueName, value); event.preventDefault(); }} onClick={(e) => e.stopPropagation()}
+                >
+
+                </Rating>
+
+
         </InteractiveCard>
-    )
-}
+
+    );
+};
--- src/components/CardPanel.tsx
+++ src/components/CardPanel.tsx
@@ -1,56 +1,75 @@
 'use client'
 import { useReducer } from "react";
 import Card from "./Card";
+import Link from "next/link";
 
 export default function CardPanel() {
-    const compareReducer = (compareList:Map<string, number>, action:{type:string, venueName:string, rating:number}) => {
-        switch(action.type) {
-            case 'add':
-                // return new Set(compareList.add(action.venueName, 0));     
-                compareList.set(action.venueName, 0);
-                return new Map(compareList);
-            case 'remove':
-                compareList.delete(action.venueName);
-                return new Map(compareList);
-            case 'update':
-                compareList.set(action.venueName, action.rating);
-                return new Map(compareList);
-            default:
-                return compareList;
-        }
-    }
 
-    const [compareList, dispatchCompare] = useReducer(compareReducer, new Map<string, number>());
+    const venueData = [
+        {vid:"001",venueName:"The Bloom Pavilion",imgSrc:"/img/bloom.jpg"},
+        {vid:"002",venueName:"Spark Space", imgSrc:"/img/sparkspace.jpg"},
+        {vid:"003",venueName:"The Grand Table", imgSrc:"/img/grandtable.jpg"}
+    ];
+
+    const venueRating = (ratingList:Map<string,number>,action:{type:string,venue:string,rating:number})=>{
+        switch (action.type){
+            case 'updateRating' :
+                const newRatingList = new Map(ratingList)
+                newRatingList.set(action.venue,action.rating??0)
+                return newRatingList
+            case 'removeRating':
+                ratingList.delete(action.venue)
+                return new Map(ratingList)
+            default :
+            return ratingList
+        }
+    };
+
+    const initialRating = new Map(venueData.map(({ venueName }) => [venueName, 0]));
+
+    const [ratingList,dispatchFunc] = useReducer(venueRating,initialRating);
 
     return (
-        <div>
-            <div style={{margin:"20px", display:"flex", flexDirection:"row", alignContent:"space-around",   justifyContent:"space-around", flexWrap:"wrap", padding:"10px"}}>
-            <Card 
-            venueName="The Bloom Pavilion" 
-            imgSrc="/img/bloom.jpg" 
-            onCompare={(venue:string) => dispatchCompare({type:'add', venueName:venue, rating:0})}
-            onRating={(venue:string, rating:number) => dispatchCompare({type:'update', venueName:venue, rating:rating})}
-            />
-            <Card
-            venueName="The Grand Table"
-            imgSrc="/img/grandtable.jpg"
-            onCompare={(venue:string) => dispatchCompare({type:'add', venueName:venue, rating:0})}
-            onRating={(venue:string, rating:number) => dispatchCompare({type:'update', venueName:venue, rating:rating})}
-            />
-            <Card
-            venueName="Spark Space"
-            imgSrc="/img/sparkspace.jpg"
-            onCompare={(venue:string) => dispatchCompare({type:'add', venueName:venue, rating:0})}
-            onRating={(venue:string, rating:number) => dispatchCompare({type:'update', venueName:venue, rating:rating})}
-            />  
-            </div> 
-            <div className="w-full text-xl font-medium">Venue List with Ratings : {compareList.size}</div>
-            {Array.from(compareList.entries()).map(([venue, rating]) => (
-                <div key={venue} onClick={() => dispatchCompare({ type: "remove", venueName: venue, rating:rating })}>
-                    {venue} : {rating}
-                </div>
-            ))}
+        <div className="flex flex-col">
+            
+            <div className="bg-slate-100 rouded-lg space-x-5 space-x-2
+          w-full px-10 py-5 flex flex-row justify-center">
+
+            {
+                venueData.map((venue)=> (
+                    <Link href={`/venue/${venue.vid}`} className="w-1/5" >
+                    
+                        <Card venueName={venue.venueName} imgSrc={venue.imgSrc}  ratingFunc={(venue:string,rating:number)=>dispatchFunc({type:'updateRating',venue:venue,rating:rating} )} 
+                            
+                            />
+                    
+                    </Link>
+                )   )
+                
+            }
+          
+            {/* <Card venueName="The Bloom Pavilion" imgSrc="/img/bloom.jpg" />
+            <Card venueName="Spark Space" imgSrc="/img/sparkspace.jpg"  />
+            <Card venueName="The Grand Table" imgSrc="/img/grandtable.jpg" /> */}
+            </div>
+        
+            <div className="p-10 bg-slate-200">
+
+            <div className=" w-full text-xl font-medium block " >
+                Venue List : {ratingList.size}
+            </div>
+
+                {Array.from(ratingList).map(([venue, rating]) => 
+                        <div data-testid={venue} key={venue} onClick={() => dispatchFunc({ type: 'removeRating', venue:venue,rating:rating })}
+                        className="block"
+                        >
+                            {venue}: {rating} 
+                        </div>
+                )}
+
+            </div>
         </div>
+            
+        
     )
-    
 }
